7527287d3ba595a48c70cde4ca5b9b059bc85a2e
amirali079 <aligoli079@gmial.com>
2022-11-16 19:04:51 +0330

    add ef core phase test codes.

---
+++ DatabaesPhases/EFTest/Model.cs
@@ -0,0 +1,71 @@
+﻿namespace EFTest;
+
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    public string DbPath { get; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Post>()
+            .HasOne(p => p.Blog)
+            .WithMany(b => b.Posts);
+        
+        modelBuilder.Entity<Blog>()
+            .Navigation(b => b.Posts)
+            .UsePropertyAccessMode(PropertyAccessMode.Property);
+        
+        modelBuilder
+            .Entity<Post>()
+            .HasMany(p => p.Tags)
+            .WithMany(p => p.Posts)
+            .UsingEntity(j => j.ToTable("PostTags"));
+       
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.
+            UseNpgsql("Host=localhost;Database=blogging3;Username=postgres;Password=root");
+
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public ICollection<Post> Posts { get; set; }
+    
+    public override string ToString()
+    {
+        return "Blog: " + BlogId + " " + Url+ " " + Posts.Count;
+    }
+    
+}
+public class Tag
+ {
+     public string TagId { get; set; }
+ 
+     public ICollection<Post> Posts { get; set; }
+ }
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public ICollection<Tag> Tags { get; set; }
+    
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+
+
---
+++ DatabaesPhases/EFTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using EFTest;
+using Microsoft.EntityFrameworkCore;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        using var db = new BloggingContext();
+
+// // Note: This sample requires the database to be created before running.
+//         Console.WriteLine($"Database path: {db.DbPath}.");
+//
+// Create
+        // Console.WriteLine("Inserting a new blog");
+        // db.Add(new Blog { Url = "http://test1.com" });
+        // db.Add(new Tag{TagId = "test1"});
+        // db.SaveChanges();
+
+       
+// Read
+        Console.WriteLine("Querying for a blog");
+        var blog = db.Blogs
+            .OrderBy(b => b.BlogId)
+            .Include(x => x.Posts)
+            .First();
+        Console.WriteLine("blog: "+blog);
+        var post = blog.Posts.First();
+        Console.WriteLine(post.PostId);
+      //  Console.WriteLine(db.Posts.First());
+        
+//
+// Update
+        // Console.WriteLine("Updating the blog and adding a post");
+        // blog.Url = "https://devblogs.microsoft.com/dotnet";
+        // blog.Posts.Add(
+        //     new Post { Title = "Hello Test", Content = "I wrote an app using EF Core!" });
+        // db.SaveChanges();
+        var tag = db.Tags.First();
+        Console.WriteLine(tag.TagId);
+        post.Tags=new List<Tag>();
+        post.Tags.Add(tag);
+        db.SaveChanges();
+
+        /*Console.WriteLine("Delete the blog");
+        db.Remove(blog);
+        db.SaveChanges();*/
+    }
+}
