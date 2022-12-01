d94ef9590cb91ace1cfd5a792f07dd3decd49a52
amirali079 <aligoli079@gmail.com>
2022-12-01 18:48:23 +0330

    add startup file

--- SearchEngine/SearchEngineWebApp/Program.cs
+++ SearchEngine/SearchEngineWebApp/Program.cs
@@ -5,44 +5,12 @@ namespace SearchEngine;
 
 internal static class Program
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddOpenApiDocument();
-        builder.Services.AddScoped<IFileReader, FileReader>();
-        builder.Services.AddScoped<IIndexer, Indexer>();
-        builder.Services.AddScoped<IQueryManager, QueryManager>();
-        builder.Services.AddScoped<IDocumentService, DocumentService>();
-
-
-        var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseSwaggerUi3();
-        app.UseOpenApi();
-
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            "default",
-            "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
+    public static void Main(string[] args) {
+        CreateHostBuilder(args).Build().Run();
     }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => {
+                webBuilder.UseStartup<Startup>();
+            });
 }
---
+++ SearchEngine/SearchEngineWebApp/Startup.cs
@@ -0,0 +1,43 @@
+﻿using SampleLibrary;
+using SearchEngine.Service;
+
+namespace SearchEngine;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddOpenApiDocument();
+        services.AddScoped<IFileReader, FileReader>();
+        services.AddScoped<IIndexer, Indexer>();
+        services.AddScoped<IQueryManager, QueryManager>();
+        services.AddScoped<IDocumentService, DocumentService>();
+    }
+    
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        app.UseSwaggerUi3();
+        app.UseOpenApi();
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
